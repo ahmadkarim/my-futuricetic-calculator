d65d6fdb6dbcb4d96c33c413a95a122b50956375
Ahmad Karim <ahmad.karim15@gmail.com>
2021-01-28 15:59:25 +0100

    removed health2 path not needed any more

--- app.py
+++ app.py
@@ -63,9 +63,6 @@ def index():
 def healthCheck():
     return "<h1>I am a healthy app!</h1>"
 
-@app.route('/health2')
-def healthCheck2():
-    return "<h1>I am a healthy app! Accenture test</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
